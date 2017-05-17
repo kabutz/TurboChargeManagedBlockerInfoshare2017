72eb5943410ec662adf48506706f6b6c24d82644
kabutz <heinz@javaspecialists.eu>
2017-05-17 11:51:17 +0200

    demo 4

--- forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
+++ forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
@@ -7,6 +7,8 @@ import java.util.concurrent.*;
 // demo1: test100_000_000() time = 46832
 // demo2: test100_000_000() time = 25230
 // demo3: test100_000_000() time = 16822
+// demo4: test100_000_000() time = 10226
+
 
 // TODO: Sign up to Heinz's "The Java Specialists' Newsletter":
 // TODO: tinyurl.com/gdansk17
@@ -15,33 +17,44 @@ public class Fibonacci {
     public static final Set<Thread> threads = ConcurrentHashMap.newKeySet();
 
     public BigInteger f(int n) {
+        Map<Integer, BigInteger> cache = new ConcurrentHashMap<>();
+        cache.put(0, BigInteger.ZERO);
+        cache.put(1, BigInteger.ONE);
+        return f(n, cache);
+    }
+
+    public BigInteger f(int n, Map<Integer, BigInteger> cache) {
         threads.add(Thread.currentThread());
-        if (n == 0) return BigInteger.ZERO;
-        if (n == 1) return BigInteger.ONE;
 
-        int half = (n + 1) / 2;
+        BigInteger result = cache.get(n);
+        if (result == null) {
 
-        RecursiveTask<BigInteger> f0_task = new RecursiveTask<BigInteger>() {
-            protected BigInteger compute() {
-                return f(half - 1);
-            }
-        };
-        f0_task.fork();
-        BigInteger f1 = f(half);
-        BigInteger f0 = f0_task.join();
+            int half = (n + 1) / 2;
 
-        long time = n > 10_000? System.currentTimeMillis() : 0;
-        try {
-            if (n % 2 == 1) {
-                return f0.multiply(f0).add(f1.multiply(f1));
-            } else {
-                return f0.shiftLeft(1).add(f1).multiply(f1);
-            }
-        } finally {
-            time = n > 10_000 ? System.currentTimeMillis() - time : 0;
-            if (time > 50) {
-                System.out.printf("f(%d) took %d ms%n", n, time);
+            RecursiveTask<BigInteger> f0_task = new RecursiveTask<BigInteger>() {
+                protected BigInteger compute() {
+                    return f(half - 1, cache);
+                }
+            };
+            f0_task.fork();
+            BigInteger f1 = f(half, cache);
+            BigInteger f0 = f0_task.join();
+
+            long time = n > 10_000 ? System.currentTimeMillis() : 0;
+            try {
+                if (n % 2 == 1) {
+                    result = f0.multiply(f0).add(f1.multiply(f1));
+                } else {
+                    result = f0.shiftLeft(1).add(f1).multiply(f1);
+                }
+                cache.put(n, result);
+            } finally {
+                time = n > 10_000 ? System.currentTimeMillis() - time : 0;
+                if (time > 50) {
+                    System.out.printf("f(%d) took %d ms%n", n, time);
+                }
             }
         }
+        return result;
     }
 }
