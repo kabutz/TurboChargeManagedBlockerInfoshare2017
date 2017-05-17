445e4058291a5fb16ba616f8f24e4942c6aa93ad
kabutz <heinz@javaspecialists.eu>
2017-05-17 11:45:00 +0200

    demo 3

--- forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
+++ forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
@@ -6,7 +6,7 @@ import java.util.concurrent.*;
 
 // demo1: test100_000_000() time = 46832
 // demo2: test100_000_000() time = 25230
-
+// demo3: test100_000_000() time = 16822
 
 // TODO: Sign up to Heinz's "The Java Specialists' Newsletter":
 // TODO: tinyurl.com/gdansk17
@@ -23,17 +23,25 @@ public class Fibonacci {
 
         RecursiveTask<BigInteger> f0_task = new RecursiveTask<BigInteger>() {
             protected BigInteger compute() {
-                return f(half-1);
+                return f(half - 1);
             }
         };
         f0_task.fork();
         BigInteger f1 = f(half);
         BigInteger f0 = f0_task.join();
 
-        if (n % 2 == 1) {
-            return f0.multiply(f0).add(f1.multiply(f1));
-        } else {
-            return f0.shiftLeft(1).add(f1).multiply(f1);
+        long time = n > 10_000? System.currentTimeMillis() : 0;
+        try {
+            if (n % 2 == 1) {
+                return f0.multiply(f0).add(f1.multiply(f1));
+            } else {
+                return f0.shiftLeft(1).add(f1).multiply(f1);
+            }
+        } finally {
+            time = n > 10_000 ? System.currentTimeMillis() - time : 0;
+            if (time > 50) {
+                System.out.printf("f(%d) took %d ms%n", n, time);
+            }
         }
     }
 }
